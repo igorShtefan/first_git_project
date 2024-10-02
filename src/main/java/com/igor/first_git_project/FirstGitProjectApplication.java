ccd953b9304e1851110674c92457e72d3120d417
User <shtefan.igo@yandex.ru>
2024-10-02 08:32:20 +0300

    Init project

---
+++ src/main/java/com/igor/first_git_project/FirstGitProjectApplication.java
@@ -0,0 +1,13 @@
+package com.igor.first_git_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstGitProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FirstGitProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/igor/first_git_project/FirstGitProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.igor.first_git_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstGitProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
